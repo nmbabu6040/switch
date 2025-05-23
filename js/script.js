f17a9be6e85454205c7ed85537c2c5b25695c02c
nmbabu6040 <nmbabu6040@gmail.com>
2025-05-23 18:31:39 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// init Isotope
+var $grid = $('#img-list').isotope({
+    // options
+  });
+  // filter items on button click
+  $('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+
+
+// slick slider
+
+  $('.sliding_part').slick({
+    dots: false,
+    infinite: false,
+    autoplay: true,
+    speed: 300,
+    arrows: true,
+    prevArrow: ('<i class="fa-solid fa-angle-double-left"></i>'),
+    nextArrow: ('<i class="fa-solid fa-angle-double-right"></i>'),
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
